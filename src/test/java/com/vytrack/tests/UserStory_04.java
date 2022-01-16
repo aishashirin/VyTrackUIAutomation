11c90dbcf8f59a6734f2b58fefdfb1ff9eedb17a
Aishashirin <aishashirina@gmail.com>
2022-01-16 17:42:48 -0600

    Created ContactsPage for UserStory_04

---
+++ src/test/java/com/vytrack/tests/UserStory_04.java
@@ -0,0 +1,9 @@
+package com.vytrack.tests;
+
+import com.vytrack.utility.TestBase;
+
+/**
+ * Story: As a user, I should be create contact information.
+ */
+public class UserStory_04 extends TestBase {
+}
