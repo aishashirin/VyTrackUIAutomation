5e7308c7ecc36e436a3948c2c56d2329f829937d
Aishashirin <aishashirina@gmail.com>
2022-01-20 17:54:39 -0600

    create VehicleCostsPage and write test in UserStory_07

--- src/test/java/com/vytrack/tests/UserStory_07.java
+++ src/test/java/com/vytrack/tests/UserStory_07.java
@@ -1,4 +1,30 @@
 package com.vytrack.tests;
 
-public class UserStory_07 {
+import com.vytrack.pages.*;
+import com.vytrack.utility.TestBase;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+
+public class UserStory_07 extends TestBase {
+    @Test
+    public void AC_01(){
+        CommonAreaPage commonAreaPage = new CommonAreaPage();
+
+        new LoginPage().login("storemanager");
+
+        commonAreaPage.navigateToModule("Fleet","Vehicle Costs");
+        commonAreaPage.waitUntilLoaderScreenDisappear();
+
+        new VehicleCostsPage().createVehicleCosts.click();
+
+        CreateVehicleCosts createVehicleCosts = new CreateVehicleCosts();
+        createVehicleCosts.fillForm();
+        createVehicleCosts.saveAndClose.click();
+
+        String actualText = createVehicleCosts.successMessage.getText();
+
+        Assertions.assertEquals("Entity saved",actualText);
+    }
+
 }
