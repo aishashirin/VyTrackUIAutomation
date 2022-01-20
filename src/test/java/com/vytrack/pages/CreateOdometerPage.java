900b216046ff7b22334deccb4da7ca0b9eb7ec39
Aishashirin <aishashirina@gmail.com>
2022-01-20 17:58:17 -0600

    create VehicleOdometerPage and CreateOdometerPage

---
+++ src/test/java/com/vytrack/pages/CreateOdometerPage.java
@@ -0,0 +1,31 @@
+package com.vytrack.pages;
+
+import com.vytrack.utility.Driver;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class CreateOdometerPage {
+    public CreateOdometerPage() {
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
+    @FindBy(css = "[data-ftid='custom_entity_type_OdometerValue']")
+    public WebElement odometerValue;
+
+    @FindBy(className = "datepicker-input")
+    public WebElement datePicker;
+
+    @FindBy(xpath = "//button[normalize-space()='Save and Close']")
+    public WebElement saveAndClose;
+
+    @FindBy(className = "message")
+    public WebElement successMessage;
+
+    public void fillForm() {
+
+        odometerValue.sendKeys("140000");
+        datePicker.sendKeys("Oct 12, 2021"+ Keys.TAB);
+    }
+}
