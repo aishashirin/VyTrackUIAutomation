fa7b5d6c8ffdae904d6fdeecccc5259e35b7c3fc
Aishashirin <aishashirina@gmail.com>
2022-01-20 17:47:41 -0600

    extend testBase in UserStory_06

--- src/test/java/com/vytrack/tests/UserStory_06.java
+++ src/test/java/com/vytrack/tests/UserStory_06.java
@@ -2,10 +2,11 @@ package com.vytrack.tests;
 
 import com.vytrack.pages.CommonAreaPage;
 import com.vytrack.pages.LoginPage;
+import com.vytrack.utility.TestBase;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-public class UserStory_06 {
+public class UserStory_06 extends TestBase {
 
     @Test
     public void AC_01(){
